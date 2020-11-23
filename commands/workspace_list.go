c5b68fdf115573ef0cadbf406c2e8b47bc6d9af6
chroju <chroju@users.noreply.github.com>
2020-11-23 19:48:03 +0900

    Add workspace list command output option

--- commands/workspace_list.go
+++ commands/workspace_list.go
@@ -2,12 +2,14 @@ package commands
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"strings"
 	"text/tabwriter"
 
 	"github.com/chroju/tfcloud/tfc"
 	"github.com/mitchellh/cli"
+	flag "github.com/spf13/pflag"
 )
 
 type WorkspaceListCommand struct {
@@ -15,7 +17,7 @@ type WorkspaceListCommand struct {
 }
 
 func (c *WorkspaceListCommand) Run(args []string) int {
-	if len(args) != 3 {
+	if len(args) < 3 {
 		c.UI.Error("Arguments is not valid.")
 		c.UI.Info(c.Help())
 		return 1
@@ -29,20 +31,45 @@ func (c *WorkspaceListCommand) Run(args []string) int {
 		return 1
 	}
 
+	buf := &bytes.Buffer{}
+	var format string
+	f := flag.NewFlagSet("module_list", flag.ContinueOnError)
+	f.SetOutput(buf)
+	f.StringVar(&format, "output", "table", "output format (table, json)")
+	if err := f.Parse(args); err != nil {
+		c.UI.Info(c.Help())
+		return 1
+	}
+	if format != "table" && format != "json" {
+		c.UI.Error("--output must be 'table' or 'json'")
+		c.UI.Info(c.Help())
+		return 1
+	}
+
 	result, err := client.WorkspaceList(organization)
 	if err != nil {
 		c.UI.Error(err.Error())
 		return 1
 	}
 
-	out := new(bytes.Buffer)
-	w := tabwriter.NewWriter(out, 0, 4, 1, ' ', 0)
-	fmt.Fprintln(w, "NAME\tID\tLINK")
-	for _, r := range result {
-		fmt.Fprintf(w, "%s\t%s\thttps://%s/app/%s/workspaces/%s\n", r.Name, r.ID, address, organization, r.Name)
+	switch format {
+	case "table":
+		out := new(bytes.Buffer)
+		w := tabwriter.NewWriter(out, 0, 4, 1, ' ', 0)
+		fmt.Fprintln(w, "NAME\tID\tLINK")
+		for _, r := range result {
+			fmt.Fprintf(w, "%s\t%s\thttps://%s/app/%s/workspaces/%s\n", r.Name, r.ID, address, organization, r.Name)
+		}
+		w.Flush()
+		c.UI.Output(out.String())
+	case "json":
+		out, err := json.Marshal(result)
+		if err != nil {
+			c.UI.Error(err.Error())
+			return 1
+		}
+		c.UI.Output(string(out))
 	}
-	w.Flush()
-	c.UI.Output(out.String())
 	return 0
 }
 
