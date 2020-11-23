6febcbdf8d7d2473cb4d4d1409550c8d156ad3ba
chroju <chroju@users.noreply.github.com>
2020-11-23 19:47:35 +0900

    Add module list command output option

--- commands/module_list.go
+++ commands/module_list.go
@@ -2,12 +2,14 @@ package commands
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"strings"
 	"text/tabwriter"
 
 	"github.com/chroju/tfcloud/tfc"
 	"github.com/mitchellh/cli"
+	flag "github.com/spf13/pflag"
 )
 
 type ModuleListCommand struct {
@@ -23,21 +25,46 @@ func (c *ModuleListCommand) Run(args []string) int {
 		return 1
 	}
 
+	buf := &bytes.Buffer{}
+	var format string
+	f := flag.NewFlagSet("module_list", flag.ContinueOnError)
+	f.SetOutput(buf)
+	f.StringVar(&format, "output", "table", "output format (table, json)")
+	if err := f.Parse(args); err != nil {
+		c.UI.Info(c.Help())
+		return 1
+	}
+	if format != "table" && format != "json" {
+		c.UI.Error("--output must be 'table' or 'json'")
+		c.UI.Info(c.Help())
+		return 1
+	}
+
 	result, err := client.ModuleList()
 	if err != nil {
 		c.UI.Error(err.Error())
 		return 1
 	}
 
-	out := new(bytes.Buffer)
-	w := tabwriter.NewWriter(out, 0, 4, 1, ' ', 0)
-	fmt.Fprintln(w, "NAME\tLATEST\tLINK")
-	for _, r := range result {
-		latest := r.VersionStatuses[0].Version
-		fmt.Fprintf(w, "%s\t%s\thttps://%s/app/%s/modules/view/%s/%s/%s\n", r.Name, latest, address, r.Organization, r.Name, r.Provider, latest)
+	switch format {
+	case "table":
+		out := new(bytes.Buffer)
+		w := tabwriter.NewWriter(out, 0, 4, 1, ' ', 0)
+		fmt.Fprintln(w, "NAME\tLATEST\tLINK")
+		for _, r := range result {
+			latest := r.VersionStatuses[0].Version
+			fmt.Fprintf(w, "%s\t%s\thttps://%s/app/%s/modules/view/%s/%s/%s\n", r.Name, latest, address, r.Organization, r.Name, r.Provider, latest)
+		}
+		w.Flush()
+		c.UI.Output(out.String())
+	case "json":
+		out, err := json.Marshal(result)
+		if err != nil {
+			c.UI.Error(err.Error())
+			return 1
+		}
+		c.UI.Output(string(out))
 	}
-	w.Flush()
-	c.UI.Output(out.String())
 	return 0
 }
 
