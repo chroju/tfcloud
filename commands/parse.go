081a84d3738287763acf7400e5361a607a29bf1c
chroju <chroju@users.noreply.github.com>
2020-11-28 12:08:55 +0900

    Add tfconfig parser

--- commands/parse.go
+++ commands/parse.go
@@ -2,9 +2,16 @@ package commands
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
-	"github.com/hashicorp/hcl2/gohcl"
-	"github.com/hashicorp/hcl2/hclparse"
+	version "github.com/hashicorp/go-version"
+	hcl "github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/gohcl"
+	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/hashicorp/hcl/v2/hclwrite"
 )
 
 type terraformrc struct {
@@ -14,6 +21,14 @@ type terraformrc struct {
 type credential struct {
 	Name  string `hcl:"name,label"`
 	Token string `hcl:"token"`
+}
+
+type cliConfig struct {
+	Token           string
+	Hostname        string
+	Organization    string
+	Workspace       string
+	RequiredVersion version.Constraints
 }
 
 func ParseTerraformrc(path string) (*credential, error) {
@@ -30,4 +45,67 @@ func ParseTerraformrc(path string) (*credential, error) {
 	}
 
 	return tfrc.Credentials[0], nil
+}
+
+func parseTfRemoteBackend(root string) (*cliConfig, error) {
+	var config *cliConfig
+	err := filepath.Walk(root,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if info.IsDir() || !strings.HasSuffix(info.Name(), ".tf") {
+				return nil
+			}
+
+			src, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			file, diags := hclwrite.ParseConfig(src, path, hcl.InitialPos)
+			if diags.HasErrors() {
+				return diags
+			}
+
+			for _, block := range file.Body().Blocks() {
+				if block.Type() == "terraform" {
+					for _, subBlock := range block.Body().Blocks() {
+						if subBlock.Type() == "backend" && subBlock.Labels()[0] == "remote" {
+							subBlockBody := subBlock.Body()
+							ver, err := version.NewConstraint(parseAttribute(block.Body().GetAttribute("required_version")))
+							if err != nil {
+								return err
+							}
+							config = &cliConfig{
+								Organization:    parseAttribute(subBlockBody.GetAttribute("organization")),
+								Hostname:        parseAttribute(subBlockBody.GetAttribute("hostname")),
+								Workspace:       parseAttribute(subBlockBody.Blocks()[0].Body().GetAttribute("name")),
+								RequiredVersion: ver,
+							}
+							return nil
+						}
+					}
+				}
+			}
+			return nil
+		})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if config == nil {
+		return nil, fmt.Errorf("Remote backend config is not found")
+	}
+
+	return config, nil
+}
+
+func parseAttribute(a *hclwrite.Attribute) string {
+	if a == nil {
+		return ""
+	}
+	return string(a.Expr().BuildTokens(nil)[1].Bytes)
 }
